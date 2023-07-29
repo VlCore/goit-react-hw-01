0c29495e1ee769ed34463a4599f598f85fa15d88
VlCore <korovin_vladislav@icloud.com>
2023-07-29 23:54:10 +0300

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -36,8 +36,8 @@ TransactionHistory.propTypes = {
         PropTypes.shape({
             id: PropTypes.string,
             type: PropTypes.string,
-            amount: PropTypes.number,
-            currency: PropTypes.number,
+            amount: PropTypes.string,
+            currency: PropTypes.string,
         })
     )
 }
